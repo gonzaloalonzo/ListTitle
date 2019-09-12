5488d9a66d9e6a37fa71cdc5d13b26e8876482fa
gonzalo alonzo mancilla <gonzaloalonzo@hotmail.com>
2019-09-12 18:32:26 -0300

    navegacion con rutas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -24,14 +24,14 @@ class HomePage extends StatelessWidget {
           builder: ( context, AsyncSnapshot<List<dynamic>> snapshot){
             
             return ListView(
-              children: _listaItems(snapshot.data)
+              children: _listaItems(snapshot.data, context),
            );
           }
 
         ); 
           }
           
-            List<Widget> _listaItems(List<dynamic> data) {
+            List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
 
               final List<Widget> opciones = [];
 
@@ -42,6 +42,15 @@ class HomePage extends StatelessWidget {
                   leading: getIcon(opc['icon']),
                   trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
                   onTap: (){
+
+
+                    Navigator.pushNamed(context, opc['ruta']);
+
+                    /* final route = MaterialPageRoute(
+                      builder: (context) => AlertPage()
+                      
+                    );
+                    Navigator.push(context, route); */
 
                   },
                 );
